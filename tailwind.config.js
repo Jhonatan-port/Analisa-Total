402a1a370cabf551ecd0c539e21ad4cc56a75287
Jhonatan <jhonatansantos070@gmail.com>
2022-12-10 10:10:20 -0300

    efetuada a criação de todas as telas ja existente utilizando flask

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./templates/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'btn-padrao' : '#0497aa',
+        'btn-padrao-hover' : '#074a53'
+      },
+      height: {
+        'semi-screen' : '80vh'
+      },
+    },
   },
   plugins: [],
 }
