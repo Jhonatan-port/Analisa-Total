666f91ef915045c8ef6839b31e41a0d1165de05f
Jhonatan <jhonatansantos070@gmail.com>
2022-12-16 14:04:53 -0300

    alterando botão dropdown

--- static/script.js
+++ static/script.js
@@ -1,5 +1,25 @@
 const fileInput = document.querySelector('#custom-file-input');
 const imagePreview = document.querySelector('.image-preview');
+const menuDropdown = document.getElementById('dropdown__menu');
+const dropDownOpt = document.getElementById('dropdown__opt');
+const dropdownImage = document.getElementsByClassName('dropdown__image');
+var mostrarDropdown = false
+
+window.addEventListener('load', function(){
+menuDropdown.addEventListener('click', function(){
+    acionaDropdown()
+})
+
+function acionaDropdown(){
+    mostrarDropdown = !mostrarDropdown
+    if(mostrarDropdown){
+        dropDownOpt.classList.remove('hidden')
+    }else{
+        dropDownOpt.classList.add('hidden')
+    }
+}
+
+})
 
 fileInput.addEventListener('change', () => {
   const file = fileInput.files[0];
