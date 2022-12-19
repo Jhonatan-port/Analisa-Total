8d512fe631b300124c36c12c323055bc16f7b2c7
Jhonatan <jhonatansantos070@gmail.com>
2022-12-19 10:21:38 -0300

    melhorando as funcionalidades do dropdown

--- static/script.js
+++ static/script.js
@@ -2,21 +2,32 @@ const fileInput = document.querySelector('#custom-file-input');
 const imagePreview = document.querySelector('.image-preview');
 const menuDropdown = document.getElementById('dropdown__menu');
 const dropDownOpt = document.getElementById('dropdown__opt');
-const dropdownImage = document.getElementsByClassName('dropdown__image');
+const dropdownAtivar = document.getElementById('dropdown__Ativar');
+const dropdownAtivo = document.getElementById('dropdown__Ativo');
+const conteudo = document.getElementById('conteudo__principal');
 var mostrarDropdown = false
 
 window.addEventListener('load', function(){
 menuDropdown.addEventListener('click', function(){
     acionaDropdown()
+    
 })
 
+conteudo.addEventListener('click', function(){
+  if(mostrarDropdown){
+    acionaDropdown()
+  }
+})
+ 
 function acionaDropdown(){
     mostrarDropdown = !mostrarDropdown
-    if(mostrarDropdown){
-        dropDownOpt.classList.remove('hidden')
-    }else{
-        dropDownOpt.classList.add('hidden')
-    }
+    console.log(mostrarDropdown);
+
+
+    dropDownOpt.classList.toggle('hidden')
+    dropdownAtivo.classList.toggle('hidden')
+    dropdownAtivar.classList.toggle('hidden')
+   
 }
 
 })
